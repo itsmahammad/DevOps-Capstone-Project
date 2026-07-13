f6af36d864864a76aac36cb28982b78485e6b416
Huntless937 <fidailveliyev09.gamil.com>
2026-07-13 16:19:54 +0400

    stacc

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     # REPLACE these with the outputs from infra/terraform/bootstrap after it is applied.
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstatepszywy"
+    storage_account_name = "tfstatekb5ggp"
     container_name       = "tfstate"
     key                  = "production/terraform.tfstate"
   }
